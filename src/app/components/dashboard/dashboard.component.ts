d3ecefae03c76d919c54c731fe88e701e8432992
MGonzalesZ <mauriciogzb1322@gmail.com>
2024-02-13 23:25:25 -0400

    avance previo

---
+++ src/app/components/dashboard/dashboard.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import { NavbarComponent } from './navbar/navbar.component';
+import { MatTableModule } from '@angular/material/table';
+
+export interface Estudiante {
+  nombre: string;
+  apellido: string;
+  edad: number;
+  carrera: string;
+}
+
+const ESTUDIANTES: Estudiante[] = [
+  { nombre: 'Paola', apellido: 'Lopez', edad: 23, carrera: 'Ing. de Sistemas' },
+  { nombre: 'Paola', apellido: 'Lopez', edad: 23, carrera: 'Ing. de Sistemas' },
+  { nombre: 'Paola', apellido: 'Lopez', edad: 23, carrera: 'Ing. de Sistemas' },
+  { nombre: 'Paola', apellido: 'Lopez', edad: 23, carrera: 'Ing. de Sistemas' },
+  { nombre: 'Paola', apellido: 'Lopez', edad: 23, carrera: 'Ing. de Sistemas' },
+  { nombre: 'Paola', apellido: 'Lopez', edad: 23, carrera: 'Ing. de Sistemas' },
+  { nombre: 'Paola', apellido: 'Lopez', edad: 23, carrera: 'Ing. de Sistemas' },
+];
+
+@Component({
+  selector: 'app-dashboard',
+  standalone: true,
+  imports: [NavbarComponent, MatTableModule],
+  templateUrl: './dashboard.component.html',
+  styleUrl: './dashboard.component.css',
+})
+export class DashboardComponent {
+  nombres_columnas: string[] = ['nombre', 'apellido', 'edad', 'carrera'];
+
+  // HACER GET A ESTUDIANTES
+  lista_estudiantes = ESTUDIANTES;
+}
---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,45 @@
+import { Component } from '@angular/core';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+
+import {
+  ReactiveFormsModule,
+  FormControl,
+  FormGroup,
+  Validator,
+  Validators,
+} from '@angular/forms';
+
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [
+    MatInputModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    ReactiveFormsModule,
+  ],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css',
+})
+export class LoginComponent {
+  logo = '../../../assets/profile.png';
+
+  constructor(private router: Router) {}
+
+  formLogin = new FormGroup({
+    usuario: new FormControl('', Validators.required),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(5),
+    ]),
+  });
+
+  ingresar(): void {
+    // codigo para verificar usuario y password
+    this.router.navigateByUrl('/dashboard');
+  }
+}
